6f414bc1a7d377d3c8f935eae049fbecf18af4ab
shenjihao <1843781563@qq.com>
2022-09-27 19:50:01 +0800

    文本等间距排列

--- main.py
+++ main.py
@@ -31,18 +31,17 @@ def annotation(file_name, bg=(255, 255, 255), show=True, save=False, **kwargs):
         if k in ExifTags.TAGS
     }
 
-    # 品牌Logo
+    space = int(blank_h * 0.3)
+
+    # Logo
     if 'make' in kwargs:
         make = kwargs['make']
     else:
         make = exif['Make']
-    
     logo = Image.open('assets/{}.png'.format(make))
     logo_h = int(blank_h * kwargs['logo_scale']) 
     logo_w = int(logo.width / logo.height * logo_h)
     logo = logo.resize((logo_w, logo_h))
-
-    img_new.paste(logo, (int(blank_h * 0.3), img_h + int(blank_h * (1 - kwargs['logo_scale']) / 2)), mask=logo)
 
     # 相机型号
     if 'model' in kwargs:
@@ -50,48 +49,49 @@ def annotation(file_name, bg=(255, 255, 255), show=True, save=False, **kwargs):
     else:
         model = exif['Model']
     model = model.replace(make, '').lstrip()
-
-    draw.text((int(blank_h * 0.6 + logo_w), img_h + int(blank_h * 0.4)), model, (0, 0, 0), font=font)
-
-    # 镜头Logo
-    if 'lens_model' in kwargs:
-        lens_logo = Image.open('assets/{}.png'.format(kwargs['lens_model']))
-        lens_logo_h = int(blank_h * kwargs['logo_scale']) 
-        lens_logo_w = int(lens_logo.width / lens_logo.height * lens_logo_h)
-        lens_logo = lens_logo.resize((lens_logo_w, lens_logo_h))
-
-        img_new.paste(lens_logo, (int(blank_w * 0.53 - lens_logo_w), img_h + int(blank_h * (1 - kwargs['logo_scale']) / 2)), mask=lens_logo)
-
-    # 分割线
-    draw.line((int(blank_w * 0.55), int(img_h + 0.2 * blank_h), int(blank_w * 0.55), int(img_h + 0.8 * blank_h)), fill=(200, 200, 200), width=int(blank_w / 500))
+    bbox = font.getbbox(str(model))
+    model_w = abs(bbox[2] - bbox[0])
 
     # 焦距
     focal = '{:d}mm'.format(int(exif['FocalLength']))
-    draw.text((int(blank_w * 0.57), img_h + int(blank_h * 0.4)), focal, (0, 0, 0), font=font)
-    
-    # 光圈
-    aperture = 'f/{:.1f}'.format(float(exif['FNumber']))
     bbox = font.getbbox(str(focal))
     focal_w = abs(bbox[2] - bbox[0])
-    draw.text((int(blank_w * 0.60 + focal_w), img_h + int(blank_h * 0.4)), aperture, (0, 0, 0), font=font) 
 
-    # 快门
+    # 光圈
+    aperture = 'f/{:.1f}'.format(float(exif['FNumber']))
+    bbox = font.getbbox(str(aperture))
+    aperture_w =  abs(bbox[2] - bbox[0])
+
+     # 快门
     shutter = float(exif['ExposureTime'])
     if shutter >= 1:
         shutter = '{:d}"'.format(int(shutter))
     else:
         shutter = '1/{:d}'.format(int(1 / shutter))
-    
-    bbox = font.getbbox(str(aperture))
-    aperture_w =  abs(bbox[2] - bbox[0])
-    draw.text((int(blank_w * 0.63 + focal_w + aperture_w), img_h + int(blank_h * 0.4)), shutter, (0, 0, 0), font=font)
-
+    bbox = font.getbbox(str(shutter))
+    shutter_w = abs(bbox[2] - bbox[0])
 
     # 感光度
     iso = 'ISO {:d}'.format(exif['ISOSpeedRatings'])
-    bbox = font.getbbox(str(shutter))
-    shutter_w = abs(bbox[2] - bbox[0])
-    draw.text((int(blank_w * 0.66 + focal_w + aperture_w + shutter_w), img_h + int(blank_h * 0.4)), iso, (0, 0, 0), font=font)
+    bbox = font.getbbox(str(iso))
+    iso_w = abs(bbox[2] - bbox[0])
+
+    # 分割线
+    left_w = blank_w - int(focal_w + aperture_w + shutter_w + iso_w + space * 5)
+
+    draw.line((left_w, int(img_h + 0.2 * blank_h), left_w, int(img_h + 0.8 * blank_h)), fill=(200, 200, 200), width=int(blank_w / 500))
+
+    img_new.paste(logo, (left_w - space - logo_w, img_h + int(blank_h * (1 - kwargs['logo_scale']) / 2)), mask=logo)
+
+    draw.text((space, img_h + int(blank_h * 0.4)), model, (0, 0, 0), font=font)
+    
+    draw.text((left_w + space, img_h + int(blank_h * 0.4)), focal, (0, 0, 0), font=font)
+   
+    draw.text((left_w + focal_w + 2 * space, img_h + int(blank_h * 0.4)), aperture, (0, 0, 0), font=font) 
+   
+    draw.text((left_w + focal_w + aperture_w + 3 * space, img_h + int(blank_h * 0.4)), shutter, (0, 0, 0), font=font)
+
+    draw.text((left_w + focal_w + aperture_w + shutter_w + 4 * space, img_h + int(blank_h * 0.4)), iso, (0, 0, 0), font=font)
 
     # 是否显示
     if show:
@@ -102,10 +102,13 @@ def annotation(file_name, bg=(255, 255, 255), show=True, save=False, **kwargs):
         img_new.save('output/' + file_name.split('/')[-1], quality=100, subsampling=0)
 
 if __name__ == '__main__':
-    annotation('img/DSC04887.jpg', logo_scale=0.2, lens_model='GMaster')
+    # annotation('img/DSC04887.jpg', logo_scale=0.2, lens_model='GMaster')
 
-    annotation('img/DSC02889.jpg', logo_scale=0.2)
+    # annotation('img/DSC02889.jpg', logo_scale=0.2)
 
-    annotation('img/DJI_0398.jpg', logo_scale=0.4, make='DJI', model='Mavic2 Pro', lens_model='Hasselblad')
+    # annotation('img/DJI_0398.jpg', logo_scale=0.4, model='Mavic2 Pro')
 
-    annotation('img/IMG_00038.jpg', logo_scale=0.2)
+    annotation('img/IMG_4101.jpg', logo_scale=0.3)
+
+    # annotation('img/IMG_00038.jpg', logo_scale=0.2)
+
